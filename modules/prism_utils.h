ebb205c2a3ca8afe045cf2b7ddf3888d9e098875
C3viche <28328104+C3viche@users.noreply.github.com>
2026-03-07 22:12:25 -0800

    import stdio

--- modules/prism_utils.h
+++ modules/prism_utils.h
@@ -8,6 +8,8 @@
 #ifndef MODULES_PRISM_UTILS_H_
 #define MODULES_PRISM_UTILS_H_
 
+#include <stdio.h>
+
 // The CC3200’s Analog-to-Digital Converter (ADC) only understands positive voltages.
 // It has a 12-bit resolution, meaning it translates voltage into a strict scale from 0 to 4095
 #define MAX_ADC_VALUE ((1 << 12) - 1)
